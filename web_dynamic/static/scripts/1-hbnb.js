61d5c2964a98864f41df40230d4fbbd156ccf91e
meddizzle316 <meddizzle316@gmail.com>
2024-04-04 13:51:15 -0500

    trying to fix lack of spaces between input names

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
   
   $(':input').click(function() {
-    amenity_dict =  $(':input[type="checkbox"]');
-    console.log(amenity_dict);
+    amenity_dict =  $(':input[type="checkbox"]:checked');
+    let checkedCheckboxes = [];
+    amenity_dict.each(function() {
+      checkedCheckboxes.push($(this).attr('data-name'));
+    })
+    let resultString = "";
+    resultString = " ".concat(...checkedCheckboxes);
+    // for (string in Object.values(checkedCheckboxes)) {
+    //   console.log(typeof string === "string")
+    //   console.log(string);
+    //   resultString = [resultString, string].join(' ');
+    // }
+    console.log(resultString);
+    
   })
   
 
