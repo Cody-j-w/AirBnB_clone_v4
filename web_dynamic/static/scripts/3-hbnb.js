7a59270deaf46df227f77287a90fd45d4c84e4e7
meddizzle316 <meddizzle316@gmail.com>
2024-04-08 14:22:12 -0500

    pushing changes to 3 and 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  
   $(':input').click(function() {
     amenity_dict =  $(':input[type="checkbox"]:checked');
     let checkedCheckboxes = [];
@@ -11,9 +10,8 @@ $(document).ready(function() {
     console.log(resultString);
     $('#amenity_list').text(resultString);
   })
-
     setInterval(function () {
-    $.get('http://0.0.0.0:5001/api/v1/status/') 
+    $.get('http://0.0.0.0:5001/api/v1/status/')
     .done(function(data){
         $('div#api_status').addClass('available');
         console.log('api is available');
@@ -22,8 +20,7 @@ $(document).ready(function() {
       $('div#api_status').removeClass('available');
       console.log('api is unavailable');
     })
-  }, 30000) 
-
+  }, 30000)
   $.ajax({
     type: 'POST',
     url: "http://0.0.0.0:5001/api/v1/places_search",
@@ -36,85 +33,71 @@ $(document).ready(function() {
       }
       for (key in allPlaces) {
         let place = allPlaces[key]
-
         let $newArticle = document.createElement("article");
-
         //creating title_box and place name
-        let $title_box = $("div", {class: 'title_box'});
+        let $title_box = document.createElement("div");
+        $title_box.classList.add("title_box");
         let $placeName = document.createElement("div");
         $placeName.append(place.name);
-
+        $title_box.append($placeName);
         //creating price_by_night
-        let $price = $("div", {class: 'price_by_night'});
+        let $price = document.createElement("h2");
+        $price.classList.add("price_by_night");
         $price.append(place.price_by_night);
-        $placeName.append($price);
-
+        $title_box.append($price);
         // appending place name and title_box to newArticle
-        $newArticle.append($placeName, $title_box);
-        $('.places'$(document).ready(function() {
-  
-          $(':input').click(function() {
-            amenity_dict =  $(':input[type="checkbox"]:checked');
-            let checkedCheckboxes = [];
-            amenity_dict.each(function() {
-              checkedCheckboxes.push($(this).attr('data-name'));
-            })
-            let resultString = "";
-            resultString = checkedCheckboxes.join(', ');
-            console.log(resultString);
-            $('#amenity_list').text(resultString);
-          })
-        
-            setInterval(function () {
-            $.get('http://0.0.0.0:5001/api/v1/status/') 
-            .done(function(data){
-                $('div#api_status').addClass('available');
-                console.log('api is available');
-              })
-            .fail(function(xhr, status, error) {
-              $('div#api_status').removeClass('available');
-              console.log('api is unavailable');
-            })
-          }, 30000) 
-        
-          $.ajax({
-            type: 'POST',
-            url: "http://0.0.0.0:5001/api/v1/places_search",
-            data: JSON.stringify({}),
-            contentType: "application/json",
-            success: function(response) {
-              let allPlaces = [];
-              for (object in response) {
-                allPlaces.push(response[object])
-              }
-              for (key in allPlaces) {
-                let place = allPlaces[key]
-        
-                let $newArticle = document.createElement("article");
-        
-                //creating title_box and place name
-                let $title_box = $("div", {class: 'title_box'});
-                let $placeName = document.createElement("div");
-                $placeName.append(place.name);
-        
-                //creating price_by_night
-                let $price = $("div", {class: 'price_by_night'});
-                $price.append(place.price_by_night);
-                $placeName.append($price);
-        
-                // appending place name and title_box to newArticle
-                $newArticle.append($placeName, $title_box);
-                $('.places').append($newArticle);
-              
-        
-              }
-            }
-          })
-        }) 
-        ).append($newArticle);
-      
+        $newArticle.append($title_box);
 
+        // creating information div
+        let $information = document.createElement("div");
+        $information.classList.add("information");
+
+        // creating max_guest div 
+        let $max_guest = document.createElement("div");
+        $max_guest.classList.add("max_guest");
+        $max_guest.append(place.max_guest);
+        
+
+        // creating number_rooms and number_bathrooms
+        let $number_rooms = document.createElement("div");
+        $number_rooms.classList.add("number_rooms");
+        $number_rooms.append(place.number_rooms);
+
+        let $number_bathrooms = document.createElement("div");
+        $number_bathrooms.classList.add("number_bathrooms");
+        $number_bathrooms.append(place.number_bathrooms);
+
+        // appending number_rooms and number_bathrooms to max_guest
+        $max_guest.append($number_rooms);
+        $max_guest.append($number_bathrooms);
+
+        // appending the max_guest div to information
+        $information.append($max_guest);
+        
+        //appending information to newArticle
+        $newArticle.append($information);
+
+        // creating user div
+        // let user = document.createElement("div");
+        // user.classList.add("user");
+        // user.append(place.user.first_name);
+        // user.append(place.user.last_name);
+
+        //appending user div to newARticle
+        // $newArticle.append($user);
+
+
+        //creating description div
+        let $description = document.createElement("div");
+        $description.classList.add("description");
+        $description.append(place.description);
+
+        // appending description div to new Article
+        $newArticle.append($description)
+
+        // appending the completed newArticle to the end of the places section
+        $('.places').append($newArticle);
       }
     }
   })
-}) 
+})
---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,126 @@
+$(document).ready(function() {
+
+  setInterval(function () {
+    $.get('http://0.0.0.0:5001/api/v1/status/')
+    .done(function(data){
+        $('div#api_status').addClass('available');
+        console.log('api is available');
+      })
+    .fail(function(xhr, status, error) {
+      $('div#api_status').removeClass('available');
+      console.log('api is unavailable');
+    })
+  }, 30000);
+  function ajaxCall() {
+    return $.ajax({
+      type: 'POST',
+      url: "http://0.0.0.0:5001/api/v1/places_search",
+      data: JSON.stringify({}),
+      contentType: "application/json",
+      success: function(response) {
+        let allPlaces = [];
+        for (object in response) {
+          allPlaces.push(response[object])
+        }
+      }
+      
+    });
+  
+  } 
+  function javascriptLoop(allPlaces){
+    for (key in allPlaces) {
+      let place = allPlaces[key]
+      let $newArticle = document.createElement("article");
+      //creating title_box and place name
+      let $title_box = document.createElement("div");
+      $title_box.classList.add("title_box");
+      let $placeName = document.createElement("div");
+      $placeName.append(place.name);
+      $title_box.append($placeName);
+      //creating price_by_night
+      let $price = document.createElement("h2");
+      $price.classList.add("price_by_night");
+      $price.append(place.price_by_night);
+      $title_box.append($price);
+      // appending place name and title_box to newArticle
+      $newArticle.append($title_box);
+
+      // creating information div
+      let $information = document.createElement("div");
+      $information.classList.add("information");
+
+      // creating max_guest div 
+      let $max_guest = document.createElement("div");
+      $max_guest.classList.add("max_guest");
+      $max_guest.append(place.max_guest);
+      
+
+      // creating number_rooms and number_bathrooms
+      let $number_rooms = document.createElement("div");
+      $number_rooms.classList.add("number_rooms");
+      $number_rooms.append(place.number_rooms);
+
+      let $number_bathrooms = document.createElement("div");
+      $number_bathrooms.classList.add("number_bathrooms");
+      $number_bathrooms.append(place.number_bathrooms);
+
+      // appending number_rooms and number_bathrooms to max_guest
+      $max_guest.append($number_rooms);
+      $max_guest.append($number_bathrooms);
+
+      // appending the max_guest div to information
+      $information.append($max_guest);
+      
+      //appending information to newArticle
+      $newArticle.append($information);
+
+      // creating user div
+      // let user = document.createElement("div");
+      // user.classList.add("user");
+      // user.append(place.user.first_name);
+      // user.append(place.user.last_name);
+
+      //appending user div to newARticle
+      // $newArticle.append($user);
+
+
+      //creating description div
+      let $description = document.createElement("div");
+      $description.classList.add("description");
+      $description.append(place.description);
+
+      // appending description div to new Article
+      $newArticle.append($description)
+
+      // appending the completed newArticle to the end of the places section
+      $('.places').append($newArticle);
+      
+    }}
+  ajaxCall().then(function(data){
+    javascriptLoop(data);
+  });
+  
+  
+  $(':input').click(function() {
+    amenity_dict =  $(':input[type="checkbox"]:checked');
+    let checkedCheckboxes = [];
+    amenity_dict.each(function() {
+      checkedCheckboxes.push($(this).attr('data-name'));
+    })
+    resultString = checkedCheckboxes.join(', ');
+    console.log(resultString);
+    $('#amenity_list').text(resultString);
+    ajaxCall().then(function(data){
+      let searchResult = [];
+      for (item in data) {
+        if (item in checkedCheckboxes) {
+          searchResult.push(item);
+        }
+      }
+      javascriptLoop(searchResult);
+      console.log(searchResult);
+    });
+    
+  })
+  
+})
