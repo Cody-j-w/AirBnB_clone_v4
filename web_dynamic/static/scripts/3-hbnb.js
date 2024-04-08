e748a966a92bc0cc186d680cc9782f658c1b2f9e
Cody Walenciak <codywalenciak@gmail.com>
2024-04-08 16:10:36 -0500

    Update Places generator loop for better styling

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -33,17 +33,34 @@ $(document).ready(function() {
       }
       for (key in allPlaces) {
         let place = allPlaces[key]
+        //generate non-attribute strings from data
+        if (place.max_guest != 1) {
+          var guests = 'guests';
+        } else {
+          var guests = 'guest';
+        }
+        if (place.number_bathrooms != 1) {
+          var baths = 'baths';
+        } else {
+          var baths = 'bath';
+        }
+        if (place.number_rooms != 1) {
+          var rooms = 'rooms';
+        } else {
+          var rooms = 'room';
+        }
+
         let $newArticle = document.createElement("article");
         //creating title_box and place name
         let $title_box = document.createElement("div");
         $title_box.classList.add("title_box");
-        let $placeName = document.createElement("div");
+        let $placeName = document.createElement("h2");
         $placeName.append(place.name);
         $title_box.append($placeName);
         //creating price_by_night
         let $price = document.createElement("h2");
         $price.classList.add("price_by_night");
-        $price.append(place.price_by_night);
+        $price.append(`\$${place.price_by_night}`);
         $title_box.append($price);
         // appending place name and title_box to newArticle
         $newArticle.append($title_box);
@@ -55,25 +72,25 @@ $(document).ready(function() {
         // creating max_guest div 
         let $max_guest = document.createElement("div");
         $max_guest.classList.add("max_guest");
-        $max_guest.append(place.max_guest);
+        $max_guest.append(`${place.max_guest} ${guests}`);
         
 
         // creating number_rooms and number_bathrooms
         let $number_rooms = document.createElement("div");
         $number_rooms.classList.add("number_rooms");
-        $number_rooms.append(place.number_rooms);
+        $number_rooms.append(`${place.number_rooms} ${rooms}`);
 
         let $number_bathrooms = document.createElement("div");
         $number_bathrooms.classList.add("number_bathrooms");
-        $number_bathrooms.append(place.number_bathrooms);
-
-        // appending number_rooms and number_bathrooms to max_guest
-        $max_guest.append($number_rooms);
-        $max_guest.append($number_bathrooms);
+        $number_bathrooms.append(`${place.number_bathrooms} ${baths}`);
 
         // appending the max_guest div to information
         $information.append($max_guest);
-        
+
+        // appending number_rooms and number_bathrooms to max_guest
+        $max_guest.after($number_rooms);
+        $number_rooms.after($number_bathrooms);
+
         //appending information to newArticle
         $newArticle.append($information);
 
@@ -90,7 +107,7 @@ $(document).ready(function() {
         //creating description div
         let $description = document.createElement("div");
         $description.classList.add("description");
-        $description.append(place.description);
+        $description.innerHTML = place.description;
 
         // appending description div to new Article
         $newArticle.append($description)
